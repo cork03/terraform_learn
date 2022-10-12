e485c65d4ba20f009df3f7634f24efab5fcb5d02
住吉翼 <tsubasa.t1119@gmail.com>
2022-10-12 23:50:35 +0900

    make internet gateway

--- network.tf
+++ network.tf
@@ -120,3 +120,24 @@ resource "aws_route_table_association" "private_rt-1c" {
   route_table_id = aws_route_table.private_rt.id
   subnet_id      = aws_subnet.private-subnet-1c.id
 }
+
+# --------------
+# internet-gateway
+# --------------
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    "Name"    = "${var.project}-${var.enviroment}-igw"
+    "Project" = var.project
+    "Env"     = var.enviroment
+  }
+}
+
+resource "aws_route" "public_rt_igw_route" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+
+}
